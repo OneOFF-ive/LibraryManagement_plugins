929f916c5769c377347d15174f8a6bc12097374c
Wu <2321141691@qq.com>
2023-02-28 23:12:02 +0800

    更新接口

--- display/src/main/java/library/plugin/LibraryDisplayPlugin.java
+++ display/src/main/java/library/plugin/LibraryDisplayPlugin.java
@@ -1,13 +1,12 @@
 package library.plugin;
 
-import com.five.AddOptionAble;
 import com.five.MyCliHandle;
+import com.five.PluginService;
 import org.apache.commons.cli.Option;
 
-public class LibraryDisplayPlugin implements AddOptionAble {
-
+public class LibraryDisplayPlugin implements PluginService {
     @Override
-    public void addOption(MyCliHandle myCliHandle) {
+    public void server(MyCliHandle myCliHandle) {
         Option option = new Option("t", "test", false, "just test");
         myCliHandle.addOption(option, (Object[] args) -> {
             System.out.println("this is a test");
