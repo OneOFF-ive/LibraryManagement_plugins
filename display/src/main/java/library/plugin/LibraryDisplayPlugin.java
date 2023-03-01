bf27468448482003c0f4effb4872b53542dffb7a
Wu <2321141691@qq.com>
2023-03-01 13:04:34 +0800

    实现展示所有库存的插件

--- display/src/main/java/library/plugin/LibraryDisplayPlugin.java
+++ display/src/main/java/library/plugin/LibraryDisplayPlugin.java
@@ -7,9 +7,12 @@ import org.apache.commons.cli.Option;
 public class LibraryDisplayPlugin implements PluginService {
     @Override
     public void server(MyCliHandle myCliHandle) {
-        Option option = new Option("t", "test", false, "just test");
+        Option option = Option.builder("display")
+                .hasArg(false)
+                .desc("print all books")
+                .build();
         myCliHandle.addOption(option, (Object[] args) -> {
-            System.out.println("this is a test");
+            System.out.println(myCliHandle.getDataList());
         });
     }
 }
