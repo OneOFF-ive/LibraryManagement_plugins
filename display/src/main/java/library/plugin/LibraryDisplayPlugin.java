24a32faebd0911e661a8da6661e9d532ccdd9b4d
Wu <2321141691@qq.com>
2023-03-02 01:22:31 +0800

    更新了插件

--- display/src/main/java/library/plugin/LibraryDisplayPlugin.java
+++ display/src/main/java/library/plugin/LibraryDisplayPlugin.java
@@ -1,18 +1,22 @@
 package library.plugin;
 
+import com.five.Application;
 import com.five.MyCliHandle;
 import com.five.PluginService;
+import com.five.data.DataAccess;
 import org.apache.commons.cli.Option;
 
 public class LibraryDisplayPlugin implements PluginService {
     @Override
-    public void server(MyCliHandle myCliHandle) {
+    public void server(Application application) {
         Option option = Option.builder("display")
                 .hasArg(false)
                 .desc("print all books")
                 .build();
-        myCliHandle.addOption(option, (Object[] args) -> {
-            System.out.println(myCliHandle.getDataList());
+        application.addOption(option, (Object[] args) -> {
+            DataAccess dataAccess = application.getBookManager().getDataAccess();
+            var allData = dataAccess.getAllData();
+            System.out.println(allData);
         });
     }
 }
