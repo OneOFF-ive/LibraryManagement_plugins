ee52ebbb23fbd7f6e279d921a209c2b3bf97bffc
Wu <2321141691@qq.com>
2023-03-16 00:04:53 +0800

    实现了目标功能

--- web/src/main/java/com/five/library/WebSupplyPlugin.java
+++ web/src/main/java/com/five/library/WebSupplyPlugin.java
@@ -1,15 +1,23 @@
 package com.five.library;
 
+import com.OneFive.MyCli;
 import com.five.Application;
 import com.five.plugin.IPlugin;
 import org.apache.commons.cli.Option;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static spark.Spark.*;
 
 public class WebSupplyPlugin implements IPlugin {
+    private Application application;
+
     @Override
     public void apply(Application application) {
+        this.application = application;
+
         Option startServiceOption = Option.builder("server")
                 .hasArg(false)
                 .desc("start a web service at port 7070")
@@ -25,11 +33,21 @@ public class WebSupplyPlugin implements IPlugin {
     }
 
     void startService(Object[] args) {
-        port(7071);
+        port(7070);
         post("/", (req, res) -> {
             String content = req.body();
             String[] newArgs = content.split(" ");
-            return content;
+            MyCli myCli = application.getMyCli();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(baos);
+            PrintStream standardOut = System.out;
+            System.setOut(ps);
+
+            myCli.parseAllOptions(newArgs);
+
+            System.setOut(standardOut);
+            return baos.toString();
         });
         awaitInitialization();
     }
--- web/src/test/java/Test.java
+++ web/src/test/java/Test.java
@@ -1,5 +1,20 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static spark.Spark.*;
+
 public class Test {
     public static void main(String[] args) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        PrintStream standardOut = System.out;
+        System.setOut(ps);
+
+        System.out.println("123");
+
+        System.setOut(standardOut);
+
+        System.out.println("123312313");
 
     }
 }
